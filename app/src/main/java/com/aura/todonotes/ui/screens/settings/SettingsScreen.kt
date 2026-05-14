3a94e3fb2cd35d2d4b86eee23ab17bd32185a221
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-14 22:56:35 +0600

    Fix SettingsScreen - add Arrangement import

--- app/src/main/java/com/aura/todonotes/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/aura/todonotes/ui/screens/settings/SettingsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
