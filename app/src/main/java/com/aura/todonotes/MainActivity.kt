6d3f6c8c67f6e46678ef0d00500b34d5ab5be612
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-14 20:46:51 +0600

    Update MainActivity to integrate theme settings properly

--- app/src/main/java/com/aura/todonotes/MainActivity.kt
+++ app/src/main/java/com/aura/todonotes/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.aura.todonotes.ui.ThemeViewModel
 import com.aura.todonotes.ui.navigation.AuraNavHost
 import com.aura.todonotes.ui.theme.AuraTheme
 import dagger.hilt.android.AndroidEntryPoint
-
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AuraTheme {
+            val themeViewModel: ThemeViewModel = hiltViewModel()
+            val settings by themeViewModel.settings.collectAsState()
+
+            AuraTheme(
+                themeOption = settings.themeMode,
+                dynamicColor = true
+            ) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
