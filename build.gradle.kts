1d93db149ffc99365d0298f347996e4c061e8133
senkuboy0-cyber <senkuboy0@gmail.com>
2026-05-14 20:55:45 +0600

    Remove kotlin.android from root build.gradle.kts - built-in with AGP 9.0+

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+    // kotlin.android is built-in with AGP 9.0+
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
